db151226bbb1db93e3b8bafedf8d8e21dd6d9452
Gova <govaway@gmail.com>
2022-12-14 12:33:18 +0100

    javascript change

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const validRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const submitBtn = document.getElementById("button");
 
-
 // firstname error messages
 const empty = document.getElementById("empty");
 const than = document.getElementById("than");
@@ -25,91 +24,95 @@ const deyExceed = document.getElementById("passexceed");
 const emptyMsg = document.getElementById("deyempty");
 const deyhigherMsg = document.getElementById("deyhi");
 
- firstName.addEventListener('click',(event) => {
-    setTimeout(() => {
-      if (firstName.value == "") {
-        empty.style.display = "flex";
-        than.style.display = "none";
-        // location.reload();
-      }else if (firstName.value.length < 5) {
-        than.style.display = "flex";
-        empty.style.display = "none";
-      }else{
-        than.style.display = "none";
-        empty.style.display = "none";
-      }
-    }, 1000);
-   
- })
+firstName.addEventListener("click", (event) => {
+  setTimeout(() => {
+    if (firstName.value == "") {
+      empty.style.display = "flex";
+      than.style.display = "none";
+      // location.reload();
+    } else if (firstName.value.length < 5) {
+      than.style.display = "flex";
+      empty.style.display = "none";
+    } else {
+      than.style.display = "none";
+      empty.style.display = "none";
+    }
+  }, 5000);
+});
 
- lastNameInput.addEventListener('click', (event) => {
+lastNameInput.addEventListener("change", (event) => {
   setTimeout(() => {
     if (lastNameInput.value == "") {
       lastname.style.display = "flex";
       lasthan.style.display = "none";
-    }else if (lastNameInput.value.length < 5) {
+    } else if (lastNameInput.value.length < 5) {
       lastname.style.display = "none";
       lasthan.style.display = "flex";
-    }else {
+    } else {
       lastname.style.display = "none";
       lasthan.style.display = "none";
     }
-  }, 1000);
- })
+  }, 5000);
+});
 
- emailInput.addEventListener('click', (event) =>{
+emailInput.addEventListener("change", (event) => {
   setTimeout(() => {
     if (emailInput.value == "") {
       deyempty.style.display = "flex";
-      nocorrect.style.display="none";
-    }else if (!emailInput.value.match(validRegex)) {
-      nocorrect.style.display="flex";
+      nocorrect.style.display = "none";
+      return false;
+    } else if (!emailInput.value.match(validRegex)) {
+      nocorrect.style.display = "flex";
       deyempty.style.display = "none";
-    }else{
-      nocorrect.style.display="none";
+      return false;
+    } else {
+      nocorrect.style.display = "none";
       deyempty.style.display = "none";
+      return false;
     }
-  }, 1000);
- })
- password.addEventListener("click", (event) =>{
-    setTimeout(() => {
-      if (password.value == "") {
-        deyNull.style.display="flex";
-        deyExceed.style.display="none";
-      }else if (password.value.length < 5) {
-        deyExceed.style.display="flex";
-         deyNull.style.display="none";
-      }else {
-           deyExceed.style.display="none";
-          deyNull.style.display="none";
-      }
-    }, 1000);
- })
-
- passwordConfirmation.addEventListener('click', (event) =>{
+  }, 5000);
+});
+password.addEventListener("change", (event) => {
   setTimeout(() => {
-      if (passwordConfirmation.value == "") {
-        emptyMsg.style.display="flex";
-        deyhigherMsg.style.display="none";
-      }else if (!passwordConfirmation.value.match(password.value)) {
-        deyhigherMsg.style.display="flex";
-        emptyMsg.style.display="none";
-      }else{
-         deyhigherMsg.style.display="none";
-        emptyMsg.style.display="none";
-      }
-  }, 1000);
- })
+    if (password.value == "") {
+      deyNull.style.display = "flex";
+      deyExceed.style.display = "none";
+      return false;
+    } else if (password.value.length < 5) {
+      deyExceed.style.display = "flex";
+      deyNull.style.display = "none";
+      return false;
+    } else {
+      deyExceed.style.display = "none";
+      deyNull.style.display = "none";
+      return;
+    }
+  }, 5000);
+});
 
- submitBtn.addEventListener("click", (event) =>{
-  
-    alert("Kindly check your email for verification");
-    location.reload();
- })
-// form.addEventListener("click", (event) => {
-//   if () {
-    
-//   }
-// })
+passwordConfirmation.addEventListener("change", (event) => {
+  setTimeout(() => {
+    if (passwordConfirmation.value == "") {
+      emptyMsg.style.display = "flex";
+      deyhigherMsg.style.display = "none";
+      return false;
+    } else if (!passwordConfirmation.value.match(password.value)) {
+      deyhigherMsg.style.display = "flex";
+      emptyMsg.style.display = "none";
+    } else {
+      deyhigherMsg.style.display = "none";
+      emptyMsg.style.display = "none";
+    }
+  }, 5000);
+});
 
-// funtion
+submitBtn.addEventListener("click", (event) => {
+  console.log(
+    firstName.value +
+      lastNameInput.value +
+      emailInput.value +
+      password.value +
+      passwordConfirmation
+  );
+  location.reload();
+});
